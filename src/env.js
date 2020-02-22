16a118b3636fa6dc6c2320dd1de3687d5a47e509
marvin <yongfeide123@sina.com>
2020-02-22 20:49:49 +0800

    feat:代码梳理

--- src/env.js
+++ src/env.js
@@ -4,5 +4,5 @@ window.ENV = {
     // domain: 'http://172.19.25.6:8881',
     casDomain: '/user-login-cas-web', //cas服务
     // casDomain: 'http://127.0.0.1:8081', //cas服务
-    wgmsBaseWeb: 'http://47.106.104.22:8881',
+    wgmsBaseWeb: 'http://127.0.0.1:8881',
  }
